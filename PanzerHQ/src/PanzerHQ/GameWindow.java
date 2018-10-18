74c7b6b5eaa50cfc338c53113e41c0b13f400e18
julian helmholz <julian.helmholz@gmx.de>
2018-10-18 14:27:55 +0200

    Quit und Action Listener

--- PanzerHQ/src/PanzerHQ/GameWindow.java
+++ PanzerHQ/src/PanzerHQ/GameWindow.java
@@ -2,25 +2,68 @@
  * 
  */
 package PanzerHQ;
-
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-/**
- * @author Teilnehmer
- *
- */
+import org.omg.PortableServer.POAPackage.WrongAdapter;
+
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
 public class GameWindow extends JFrame{
-
-	public GameWindow() {
-		JFrame testpanel = new JFrame();
-		testpanel.setSize(new Dimension(600, 400));
-		add(testpanel);
-		pack();
-		testpanel.setTitle("PanzerHQ");
-		testpanel.setLocation(10, 10);
-		testpanel.setResizable(false);
-		testpanel.setVisible(true);
-	}
+    
+    public GameWindow() {       
+        
+        JPanel testPanel = new JPanel();        
+        testPanel.setPreferredSize(new Dimension(600, 400));
+        
+        createMenu();
+                
+        add(testPanel);
+        pack();
+        
+        setTitle("PanzerHQ");
+        setLocation(10, 10);
+        setResizable(false);
+        
+        setVisible(true);
+    }
+    
+    private void createMenu() {
+        
+        JMenuBar menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar);
+        
+        JMenu fileMenu = new JMenu("File");
+        JMenu gameMenu = new JMenu("Game");
+        JMenu prefMenu = new JMenu("Preferences");
+        
+        menuBar.add(fileMenu);        
+        menuBar.add(gameMenu);        
+        menuBar.add(prefMenu);
+        
+        addFileMenuItems(fileMenu);
+    }
+    
+    private void addFileMenuItems(JMenu fileMenu) {
+        
+        JMenuItem quitItem = new JMenuItem("Quit");
+        fileMenu.add(quitItem);  
+        quitItem.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+    }
+    
+    
+    
 }
---
+++ PanzerHQ/src/PanzerHQ/panzerHq.java
@@ -0,0 +1,11 @@
+package PanzerHQ;
+
+public class panzerHq {
+
+	public static void main(String[] args) {
+
+		new GameWindow();
+
+	}
+
+}
